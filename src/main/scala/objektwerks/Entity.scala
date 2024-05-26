472f2038c69f7e6a5e96c30800e18a4706f4309f
objektwerks <objektwerks@runbox.com>
2024-05-26 08:56:36 -0400

    enhanced object entity

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -1,6 +1,10 @@
 package objektwerks
 
+import java.time.LocalDate
+
 sealed trait Entity:
   val id: Long
 
-object Entity
+object Entity:
+  def now: String = LocalDate.now.toString
+  def localDate(now: String): LocalDate = if now.nonEmpty then LocalDate.parse(now) else LocalDate.now

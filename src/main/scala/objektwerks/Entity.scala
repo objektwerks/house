8519d1bcc0465f941cc7d5d11a8eda3b0f125502
objektwerks <objektwerks@runbox.com>
2024-05-26 09:54:48 -0400

    added roof entity

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -80,6 +80,11 @@ object Insulation:
   given JsonValueCodec[Insulation] = JsonCodecMaker.make[Insulation]
   given Ordering[Insulation] = Ordering.by[Insulation, String](insulation => insulation.installed).reverse
 
+final case class Roof(id: Long = 0,
+                      homeId: Long,
+                      kind: String,
+                      built: String = Entity.now) extends Entity
+
 final case class Driveway(id: Long = 0,
                           homeId: Long,
                           kind: String,

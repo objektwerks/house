47e54318f89b36bb8945a5aec80e6d0a63692dd2
objektwerks <objektwerks@runbox.com>
2024-05-26 09:21:44 -0400

    added object driveway

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -37,4 +37,9 @@ object Home:
 
 final case class Driveway(id: Long = 0,
                           homeId: Long,
-                          kind: String) extends Entity
+                          kind: String,
+                          built: String = Entity.now) extends Entity
+
+object Driveway:
+  given JsonValueCodec[Driveway] = JsonCodecMaker.make[Driveway]
+  given Ordering[Driveway] = Ordering.by[Driveway, String](driveway => driveway.built).reverse

39e58175ac4de4191099f448b851ebbc499bd2f3
objektwerks <objektwerks@runbox.com>
2024-05-28 09:14:43 -0400

    added mailbox entity

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -401,3 +401,8 @@ final case class Dock(id: Long = 0,
 object Dock:
   given JsonValueCodec[Dock] = JsonCodecMaker.make[Dock]
   given Ordering[Dock] = Ordering.by[Dock, String](dock => dock.built).reverse
+
+final case class Mailbox(id: Long = 0,
+                         homeId: Long,
+                         kind: String,
+                         built: String = Entity.now) extends Entity

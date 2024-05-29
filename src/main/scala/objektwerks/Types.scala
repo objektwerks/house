2f25e952820d945cd866400ca2df4e426af0e3a1
objektwerks <objektwerks@runbox.com>
2024-05-29 11:23:20 -0400

    extracted entity types from entity into types

---
+++ src/main/scala/objektwerks/Types.scala
@@ -0,0 +1,37 @@
+package objektwerks
+
+enum HouseType:
+  case primary, secondary, vacation
+
+enum FoundationType:
+  case slab, basement, wood, crawl
+
+enum FrameType:
+  case platform, steel
+
+enum AtticType:
+  case finished, unfinished, scuttle
+
+enum RoofType:
+  case alluminum, galvalume, tile, shingle
+
+enum ChimneyType:
+  case masonry, metal
+
+enum DrivewayType:
+  case asphalt, concrete, gravel, paver
+
+enum GarageType:
+  case attached, detached
+
+enum RoomType:
+  case kitchen, bathroom, bedroom, living, dining, den, study, laundry, office, closet
+
+enum WindowType:
+  case single, double, bay, slide
+
+enum DoorType:
+  case wood, fiberglass, steel, glass
+
+enum FloorType:
+  case carpet, concrete, tile, wood, synthetic

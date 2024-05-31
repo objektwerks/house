a5df6a3f347912041e7ae2ca08f87b84f783a799
objektwerks <objektwerks@runbox.com>
2024-05-31 04:06:21 -0400

    enhanced store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -1,3 +1,12 @@
 package objektwerks
 
-final class Store
+import com.typesafe.config.Config
+
+import scalikejdbc.*
+
+final class Store(config: Config):
+  val url = config.getString("db.url")
+  val user = config.getString("db.user")
+  val password = config.getString("db.password")
+
+  ConnectionPool.singleton(url, user, password)

93ecd2e99cea40589b14a6854853f334113b654c
objektwerks <objektwerks@runbox.com>
2024-05-31 04:11:11 -0400

    added login to store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -12,3 +12,18 @@ final class Store(config: Config):
   ConnectionPool.singleton(url, user, password)
 
   def register(account: Account): Account = addAccount(account)
+
+  def login(email: String, pin: String): Option[Account] =
+    DB readOnly { implicit session =>
+      sql"select * from account where email = $email and pin = $pin"
+        .map(rs =>
+          Account(
+            rs.long("id"),
+            rs.string("license"),
+            rs.string("email"),
+            rs.string("pin"),
+            rs.string("activated")
+          )
+        )
+        .single()
+    }

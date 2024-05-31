b6e0264aaee8dcf1241bc0c98af95aa779d4ae2f
objektwerks <objektwerks@runbox.com>
2024-05-31 05:04:47 -0400

    added store test

---
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -0,0 +1,8 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+final class StoreTest extends AnyFunSuite with Matchers:
+  test("pin"):
+    true

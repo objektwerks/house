f380418433f06da730e7daa2f011dda0e0e7a9fd
objektwerks <objektwerks@runbox.com>
2024-06-06 05:50:39 -0400

    added door table

--- ddl.sql
+++ ddl.sql
@@ -128,3 +128,10 @@ CREATE TABLE window (
   typeof VARCHAR NOT NULL,
   installed VARCHAR NOT NULL
 );
+
+CREATE TABLE door (
+  id BIGSERIAL PRIMARY KEY,
+  house_id BIGINT REFERENCES house(id),
+  typeof VARCHAR NOT NULL,
+  installed VARCHAR NOT NULL
+);

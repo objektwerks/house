59ceb7ce4d53c2fa360e767e1882a6327eee648e
objektwerks <objektwerks@runbox.com>
2024-06-11 14:58:34 -0400

    enhanced test store

--- src/test/scala/objektwerks/StoreTest.scala
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -49,6 +49,9 @@ final class StoreTest extends AnyFunSuite with Matchers:
     updateRoof( addRoof() )
     listRoofs()
 
+    updateChimney( addChimney() )
+    listChimneys()
+
   def register(): Unit =
     val id = store.register(testAccount)
     id > 0 shouldBe true

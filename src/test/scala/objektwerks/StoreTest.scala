4000239112cc53f8fb96e21815d6d392b1e262da
objektwerks <objektwerks@runbox.com>
2024-06-12 12:57:22 -0400

    added update window test

--- src/test/scala/objektwerks/StoreTest.scala
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -298,3 +298,6 @@ final class StoreTest extends AnyFunSuite with Matchers:
     val id = store.addWindow(add)
     id > 0 shouldBe true
     add.copy(id = id)
+
+  def updateWindow(update: Window): Unit =
+    store.updateWindow( update.copy(typeof = WindowType.bay) ) shouldBe 1

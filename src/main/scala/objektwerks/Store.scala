9071e79b32ed40e49a611e7598b818ea848c8606
objektwerks <objektwerks@runbox.com>
2024-06-17 08:44:44 -0400

    enhanced list frames

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -150,6 +150,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             FrameType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("built")
           )
         )

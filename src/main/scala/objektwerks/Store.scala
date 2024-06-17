681d17bc3caf9cc05d5feaf8ebf99b6b9d624a7d
objektwerks <objektwerks@runbox.com>
2024-06-17 09:27:39 -0400

    enhanced list chimneys

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -354,6 +354,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             ChimneyType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("built")
           )
         )

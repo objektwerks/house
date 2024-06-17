416226477679ebcc9e530835db22035a495c04d2
objektwerks <objektwerks@runbox.com>
2024-06-17 09:31:36 -0400

    enhanced list balconys

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -388,6 +388,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             BalconyType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("built")
           )
         )

f4746cad40a6d2e7396504055d444f41da9c95d8
objektwerks <objektwerks@runbox.com>
2024-06-17 08:33:58 -0400

    added note to house

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -28,6 +28,7 @@ final case class House(id: Long = 0,
                        accountId: Long,
                        typeof: HouseType = HouseType.primary,
                        location: String,
+                       note: String,
                        built: String = Entity.now) extends Entity
 
 object House:

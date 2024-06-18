e3fba1d0ebfa337b8d0da6f5200a78d5aea18a1f
objektwerks <objektwerks@runbox.com>
2024-06-18 10:38:23 -0400

    enhanced list windows

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -663,6 +663,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             WindowType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("installed")
           )
         )

5aa9e58b07ace4cad63e22640f4d36749fd06a59
objektwerks <objektwerks@runbox.com>
2024-06-18 10:27:40 -0400

    enhanced list sidings

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -561,6 +561,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             SidingType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("installed")
           )
         )

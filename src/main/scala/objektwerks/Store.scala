d51b863238215bd68c7ed316b75393d6f4006244
objektwerks <objektwerks@runbox.com>
2024-06-18 10:45:16 -0400

    enhanced list plumbings

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -731,6 +731,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             PlumbingType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("installed")
           )
         )

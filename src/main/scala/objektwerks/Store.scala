be089e675770335b6c6aa44d7692e118c50369d2
objektwerks <objektwerks@runbox.com>
2024-06-18 09:58:55 -0400

    enhanced list rooms

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -456,6 +456,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             RoomType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("built")
           )
         )

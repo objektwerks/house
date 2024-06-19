0a860e328bd8804989dfca7569117e33eaaeef3c
objektwerks <objektwerks@runbox.com>
2024-06-19 14:40:24 -0400

    enhanced list lightings

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -969,6 +969,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             LightingType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("installed")
           )
         )

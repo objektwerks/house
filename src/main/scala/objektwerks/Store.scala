6d76d9dee02b80eeec3b2d13bebf3b963dff1753
objektwerks <objektwerks@runbox.com>
2024-06-19 14:29:09 -0400

    enhanced list heaters

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -867,6 +867,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             HeaterType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("installed")
           )
         )

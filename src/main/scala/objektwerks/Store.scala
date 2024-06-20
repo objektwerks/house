a6ce3f67815228ead2aebdcce46b2787936f02ad
objektwerks <objektwerks@runbox.com>
2024-06-20 06:44:01 -0400

    enhanced update house

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -103,8 +103,8 @@ final class Store(config: Config,
   def updateHouse(house: House): Int =
     DB localTx { implicit session =>
       sql"""
-        update house set typeof = ${house.typeof.toString}, location = ${house.location}, note = ${house.note}, built = ${house.built}
-        where id = ${house.id}
+        update house set typeof = ${house.typeof.toString}, location = ${house.location}, label = ${house.label}, note = ${house.note},
+        built = ${house.built} where id = ${house.id}
         """
         .update()
     }

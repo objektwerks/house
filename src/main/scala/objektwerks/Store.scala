78afda8b1ee8127970b7e50287c02e10af3d4edf
objektwerks <objektwerks@runbox.com>
2024-06-20 06:17:22 -0400

    enhanced list sheds

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -1241,6 +1241,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             ShedType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("built")
           )
         )

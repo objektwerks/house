a5ae5b7771ff9fe102428d741f02df7915a4d4f3
objektwerks <objektwerks@runbox.com>
2024-06-20 06:39:10 -0400

    enhanced list mailboxes

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -1482,6 +1482,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             MailboxType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("installed")
           )
         )

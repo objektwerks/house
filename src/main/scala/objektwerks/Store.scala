af52fef0ad5a5fb8c965adf6af07dbb7c005be7e
objektwerks <objektwerks@runbox.com>
2024-06-20 06:37:05 -0400

    enhanced list gazebos

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -1448,6 +1448,8 @@ final class Store(config: Config,
             rs.long("id"),
             rs.long("house_id"),
             GazeboType.valueOf( rs.string("typeof") ),
+            rs.string("label"),
+            rs.string("note"),
             rs.string("built")
           )
         )

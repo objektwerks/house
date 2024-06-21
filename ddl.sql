cde20361dff5ef616646be8c858f7583f7dc05e9
objektwerks <objektwerks@runbox.com>
2024-06-21 06:52:04 -0400

    edited chimney table

--- ddl.sql
+++ ddl.sql
@@ -86,6 +86,8 @@ CREATE TABLE chimney (
   id BIGSERIAL PRIMARY KEY,
   house_id BIGINT REFERENCES house(id),
   typeof VARCHAR NOT NULL,
+  label VARCHAR NOT NULL,
+  note VARCHAR NOT NULL,
   built VARCHAR NOT NULL
 );
 

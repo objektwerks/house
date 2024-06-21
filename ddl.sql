ddea85f945ae604f271e01b9356312336223da63
objektwerks <objektwerks@runbox.com>
2024-06-21 06:49:25 -0400

    edited frame table

--- ddl.sql
+++ ddl.sql
@@ -32,6 +32,8 @@ CREATE TABLE frame (
   id BIGSERIAL PRIMARY KEY,
   house_id BIGINT REFERENCES house(id),
   typeof VARCHAR NOT NULL,
+  label VARCHAR NOT NULL,
+  note VARCHAR NOT NULL,
   built VARCHAR NOT NULL
 );
 

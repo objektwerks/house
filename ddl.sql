4fbb220b12e7bb42386373395896390228f5bfae
objektwerks <objektwerks@runbox.com>
2024-06-21 06:56:36 -0400

    edited alarm table

--- ddl.sql
+++ ddl.sql
@@ -213,6 +213,8 @@ CREATE TABLE alarm (
   id BIGSERIAL PRIMARY KEY,
   house_id BIGINT REFERENCES house(id),
   typeof VARCHAR NOT NULL,
+  typeof VARCHAR NOT NULL,
+  label VARCHAR NOT NULL,
   installed VARCHAR NOT NULL
 );
 

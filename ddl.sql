ab669c894fae72d21121d99020d468240249c983
objektwerks <objektwerks@runbox.com>
2024-06-21 07:00:25 -0400

    edited lawn table

--- ddl.sql
+++ ddl.sql
@@ -305,6 +305,8 @@ CREATE TABLE lawn (
   id BIGSERIAL PRIMARY KEY,
   house_id BIGINT REFERENCES account(id),
   typeof VARCHAR NOT NULL,
+  label VARCHAR NOT NULL,
+  note VARCHAR NOT NULL,
   planted VARCHAR NOT NULL
 );
 

c236008e4312f778fb07787e23b4b787b3a1e722
objektwerks <objektwerks@runbox.com>
2024-06-21 06:52:21 -0400

    edited balcony table

--- ddl.sql
+++ ddl.sql
@@ -95,6 +95,8 @@ CREATE TABLE balcony (
   id BIGSERIAL PRIMARY KEY,
   house_id BIGINT REFERENCES house(id),
   typeof VARCHAR NOT NULL,
+  label VARCHAR NOT NULL,
+  note VARCHAR NOT NULL,
   built VARCHAR NOT NULL
 );
 

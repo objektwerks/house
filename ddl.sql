a7b2db340dcb82b634abe55952c535859aa48f46
objektwerks <objektwerks@runbox.com>
2024-06-21 07:01:10 -0400

    edited sprinkler table

--- ddl.sql
+++ ddl.sql
@@ -323,6 +323,8 @@ CREATE TABLE sprinkler (
   id BIGSERIAL PRIMARY KEY,
   house_id BIGINT REFERENCES account(id),
   typeof VARCHAR NOT NULL,
+  label VARCHAR NOT NULL,
+  note VARCHAR NOT NULL,
   installed VARCHAR NOT NULL
 );
 

adb4a449044b7238613d0b966df0ed6da13b65ae
objektwerks <objektwerks@runbox.com>
2024-06-21 06:51:08 -0400

    edited ductwork table

--- ddl.sql
+++ ddl.sql
@@ -59,6 +59,8 @@ CREATE TABLE ductwork (
   id BIGSERIAL PRIMARY KEY,
   house_id BIGINT REFERENCES house(id),
   typeof VARCHAR NOT NULL,
+  label VARCHAR NOT NULL,
+  note VARCHAR NOT NULL,
   installed VARCHAR NOT NULL
 );
 

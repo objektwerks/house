a7f7cd1ce3db2ce44e1952e4e4cf63d883ab0b38
objektwerks <objektwerks@runbox.com>
2024-06-21 06:55:34 -0400

    edited plumbing table

--- ddl.sql
+++ ddl.sql
@@ -186,6 +186,8 @@ CREATE TABLE plumbing (
   id BIGSERIAL PRIMARY KEY,
   house_id BIGINT REFERENCES house(id),
   typeof VARCHAR NOT NULL,
+  typeof VARCHAR NOT NULL,
+  label VARCHAR NOT NULL,
   installed VARCHAR NOT NULL
 );
 

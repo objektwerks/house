5aa7af39846cb00497c2ef0c386d5dff5af2e630
objektwerks <objektwerks@runbox.com>
2024-06-22 09:04:53 -0400

    added account ordering

--- src/main/scala/objektwerks/Sorter.scala
+++ src/main/scala/objektwerks/Sorter.scala
@@ -1,4 +1,5 @@
 package objektwerks
 
 object Sorter:
+  given Ordering[Account] = Ordering.by[Account, String](account => account.activated).reverse
   given Ordering[House] = Ordering.by[House, String](home => home.built).reverse

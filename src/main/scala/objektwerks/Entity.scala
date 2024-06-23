56368ea2aec24a800017da849d24c36cc95d747b
objektwerks <objektwerks@runbox.com>
2024-06-23 07:24:00 -0400

    moved drywall json serializer

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -100,7 +100,6 @@ final case class Drywall(id: Long = 0,
                          built: String = Entity.now) extends Entity
 
 object Drywall:
-  given JsonValueCodec[Drywall] = JsonCodecMaker.make[Drywall]
   given Ordering[Drywall] = Ordering.by[Drywall, String](drywall => drywall.built).reverse
 
 final case class Room(id: Long = 0,

68b448ca073545b45431b173b8e497bbcdc3bd18
objektwerks <objektwerks@runbox.com>
2024-06-24 11:05:04 -0400

    enhanced validator

--- src/main/scala/objektwerks/Validator.scala
+++ src/main/scala/objektwerks/Validator.scala
@@ -1,3 +1,8 @@
 package objektwerks
 
 object Validator:
+  extension (value: String)
+    def isEmptyOrNonEmpty: Boolean = value.isEmpty || value.nonEmpty
+    def isLicense: Boolean = if value.nonEmpty && value.length == 36 then true else false
+    def isPin: Boolean = value.length == 7
+    def isEmail: Boolean = value.nonEmpty && value.length >= 3 && value.contains("@")

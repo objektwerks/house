cd022fcf6722d42fd46b8ea4c5c8c56d33dc9c94
objektwerks <objektwerks@runbox.com>
2024-06-27 06:37:27 -0400

    added save entity command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -5,3 +5,5 @@ sealed trait Command
 sealed trait License
 
 final case class ListEntity(license: String, clazz: String) extends Command with License
+
+final case class SaveEntity(license: String, entity: Entity) extends Command with License

5952ce826a4c7d1203967096d05b15330d40b1db
objektwerks <objektwerks@runbox.com>
2024-06-27 06:38:37 -0400

    enhanced license

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -2,7 +2,8 @@ package objektwerks
 
 sealed trait Command
 
-sealed trait License
+sealed trait License:
+  val license: String
 
 final case class ListEntity(license: String, clazz: String) extends Command with License
 

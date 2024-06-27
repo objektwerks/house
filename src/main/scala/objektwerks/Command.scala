f43e9eb87d4d62c7253af8c12fb8a29ea29fe508
objektwerks <objektwerks@runbox.com>
2024-06-27 06:38:59 -0400

    added register command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -5,6 +5,8 @@ sealed trait Command
 sealed trait License:
   val license: String
 
+final case class Register(email: String) extends Command
+
 final case class ListEntity(license: String, clazz: String) extends Command with License
 
 final case class SaveEntity(license: String, entity: Entity) extends Command with License

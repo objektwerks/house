94c6b155bcec84130268e44520035921f1a1dc67
objektwerks <objektwerks@runbox.com>
2024-06-27 06:41:40 -0400

    added loggedin event

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -5,3 +5,6 @@ sealed trait Event
 final case class Authorized(isAuthorized: Boolean) extends Event
 
 final case class Registered(account: Account) extends Event
+
+final case class LoggedIn(account: Account) extends Event
+

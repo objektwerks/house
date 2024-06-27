1bee63ca2990111a404f10332559d033324539cf
objektwerks <objektwerks@runbox.com>
2024-06-27 06:39:27 -0400

    added login command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -7,6 +7,8 @@ sealed trait License:
 
 final case class Register(email: String) extends Command
 
+final case class Login(email: String, pin: String) extends Command
+
 final case class ListEntity(license: String, clazz: String) extends Command with License
 
 final case class SaveEntity(license: String, entity: Entity) extends Command with License

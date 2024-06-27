e5642875b028ba1c7fb61bc38c7d322e1412bd12
objektwerks <objektwerks@runbox.com>
2024-06-27 13:49:13 -0400

    enhanced command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -9,8 +9,8 @@ final case class Register(email: String) extends Command
 
 final case class Login(email: String, pin: String) extends Command
 
-final case class ListEntity(license: String, entityType: EntityType, accountId: Long) extends Command with License
+final case class ListEntity(license: String, typeof: EntityType, accountId: Long) extends Command with License
 
-final case class AddEntity(license: String, entityType: EntityType, entity: Entity) extends Command with License
+final case class AddEntity(license: String, typeof: EntityType, entity: Entity) extends Command with License
 
-final case class UpdateEntity(license: String, entityType: EntityType, entity: Entity) extends Command with License
+final case class UpdateEntity(license: String, typeof: EntityType, entity: Entity) extends Command with License

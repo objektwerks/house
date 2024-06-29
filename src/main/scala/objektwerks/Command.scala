c459090279db1e1708fa6a874978dfa27997c833
objektwerks <objektwerks@runbox.com>
2024-06-29 11:12:13 -0400

    refactored list entities command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -9,7 +9,7 @@ final case class Register(email: String) extends Command
 
 final case class Login(email: String, pin: String) extends Command
 
-final case class ListEntities(license: String, typeof: EntityType, accountId: Long) extends Command with License
+final case class ListEntities(license: String, typeof: EntityType, houseId: Long) extends Command with License
 
 final case class AddEntity(license: String, typeof: EntityType, entity: Entity) extends Command with License
 

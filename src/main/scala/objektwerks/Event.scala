f8428c227dda75e37d6788ae4f37b4fe8413fec1
objektwerks <objektwerks@runbox.com>
2024-06-29 11:37:30 -0400

    added faults listed event

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -8,9 +8,9 @@ final case class Authorized(isAuthorized: Boolean) extends Event
 
 final case class Registered(account: Account) extends Event
 
-final case class HousesListed(houses: List[House]) extends Event
-
 final case class LoggedIn(account: Account) extends Event
+
+final case class HousesListed(houses: List[House]) extends Event
 
 final case class EntitiesListed(entities: List[Entity]) extends Event
 
@@ -19,3 +19,5 @@ final case class EntityAdded(id: Long) extends Event
 final case class EntityUpdated(count: Int) extends Event
 
 final case class Fault(cause: String, occurred: String = LocalDate.now.toString) extends Event
+
+final case class FaultsListed(faults: List[Fault]) extends Event

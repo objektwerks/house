15dff440ca63393e064087c05b4629bfa91972d5
objektwerks <objektwerks@runbox.com>
2024-06-29 11:13:08 -0400

    added list houses command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -9,6 +9,8 @@ final case class Register(email: String) extends Command
 
 final case class Login(email: String, pin: String) extends Command
 
+final case class ListHouses(license: String, accountId: Long) extends Command with License
+
 final case class ListEntities(license: String, typeof: EntityType, houseId: Long) extends Command with License
 
 final case class AddEntity(license: String, typeof: EntityType, entity: Entity) extends Command with License

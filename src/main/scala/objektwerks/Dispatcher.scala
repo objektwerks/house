04aa5e6c4102f5a9df7d2ef4c17b7a5745bb7a1d
objektwerks <objektwerks@runbox.com>
2024-06-29 08:43:47 -0400

    enhanced dispatcher dispatch def

--- src/main/scala/objektwerks/Dispatcher.scala
+++ src/main/scala/objektwerks/Dispatcher.scala
@@ -1,8 +1,30 @@
 package objektwerks
 
+import scala.util.Try
+import scala.util.control.NonFatal
+
+import Validator.*
+
 final class Dispatcher(emailer: Emailer,
                        store: Store):
-  def dispatch(command: Command): Event = ???
+  def dispatch(command: Command): Event =
+    if !command.isValid then store.addFault( Fault(s"Command is invalid: $command") )
+
+    isAuthorized(command) match
+      case Authorized(isAuthorized) => if !isAuthorized then store.addFault( Fault(s"License is unauthorized: $command") )
+      case fault @ Fault => store.addFault(fault)
+      case _ =>
+
+    val event = command match
+      case Register(emailAddress)           => register(emailAddress)
+      case Login(emailAddress, pin)         => login(emailAddress, pin)
+      case ListEntity(_, typeof, accountId) => listProperties(accountId)
+      case AddEntity(_, typeof, entity)     => saveProperty(property)
+      case UpdateEntity(_, typeof, entity)  => listSessions(propertyId)
+
+    event match
+      case fault @ Fault => store.addFault(fault)
+      case _ => event
 
   private def isAuthorized(command: Command): Event =
     command match

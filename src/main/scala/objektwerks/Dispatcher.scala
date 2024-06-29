b8d58bc0db9ff2e0979f106bae3aba3998f84927
objektwerks <objektwerks@runbox.com>
2024-06-29 08:34:02 -0400

    added is authorized def to dispatcher

--- src/main/scala/objektwerks/Dispatcher.scala
+++ src/main/scala/objektwerks/Dispatcher.scala
@@ -3,3 +3,12 @@ package objektwerks
 final class Dispatcher(emailer: Emailer,
                        store: Store):
   def dispatch(command: Command): Event = ???
+
+  private def isAuthorized(command: Command): Event =
+    command match
+      case license: License =>
+        Try {
+          Authorized( store.isAuthorized(license.license) )
+        }.recover { case NonFatal(error) => Fault(s"Authorization failed: $error") }
+         .get
+      case Register(_) | Login(_, _) => Authorized(true)

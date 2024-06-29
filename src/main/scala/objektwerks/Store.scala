990f6d10104123cb53ff4c02391c56350af3287c
objektwerks <objektwerks@runbox.com>
2024-06-29 08:49:23 -0400

    added add fault to store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -1519,3 +1519,11 @@ final class Store(config: Config,
       )
       .list()
   }
+
+  def addFault(fault: Fault): Fault = DB localTx { implicit session =>
+    sql"""
+      insert into fault(cause, occurred) values(${fault.cause}, ${fault.occurred})
+      """
+      .update()
+      fault
+  }

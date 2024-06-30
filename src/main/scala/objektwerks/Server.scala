b2fe1322f910892f3885b51f3c49fc72259a9a66
objektwerks <objektwerks@runbox.com>
2024-06-30 13:05:19 -0400

    added server

---
+++ src/main/scala/objektwerks/Server.scala
@@ -0,0 +1,39 @@
+package objektwerks
+
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
+
+import io.helidon.webserver.WebServer
+import io.helidon.webserver.http.HttpRouting
+
+import scala.concurrent.duration.DurationInt
+
+object Server extends LazyLogging:
+  @main def main(): Unit =
+    val config = ConfigFactory.load("server.conf")
+    val host = config.getString("server.host")
+    val port = config.getInt("server.port")
+    val endpoint = config.getString("server.endpoint")
+
+    val store = Store(config, Store.cache(initialSize = 4, maxSize = 10, expireAfter = 24.hour))
+    val emailer = Emailer(config)
+    val handler = Handler(store, emailer)
+    val dispatcher = Dispatcher(handler)
+
+    //val handler = Handler(dispatcher, store, logger)
+
+    val builder = HttpRouting
+      .builder
+      .post(endpoint, handler)
+
+    WebServer
+      .builder
+      .port(port)
+      .routing(builder)
+      .build
+      .start
+
+    println(s"*** Press Control-C to shutdown House Http Server at: $host:$port$endpoint")
+    logger.info(s"*** House Http Server started at: $host:$port$endpoint")
+
+    Thread.currentThread().join()

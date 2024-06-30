fd7e897b1af142cd949e340e52c862f2fe09b5da
objektwerks <objektwerks@runbox.com>
2024-06-30 10:13:26 -0400

    integrated dispatcher and handler

--- src/main/scala/objektwerks/Dispatcher.scala
+++ src/main/scala/objektwerks/Dispatcher.scala
@@ -17,8 +17,8 @@ final class Dispatcher(emailer: Emailer,
       case _ =>
 
     val event = command match
-      case Register(emailAddress)           => register(emailAddress)
-      case Login(emailAddress, pin)         => login(emailAddress, pin)
+      case Register(emailAddress)           => handler.register(emailAddress)
+      case Login(emailAddress, pin)         => handler.login(emailAddress, pin)
       case ListHouses(_, accountId)         => handler.listHouses(accountId)
       case ListEntities(_, typeof, houseId) => handler.listEntities(typeof, houseId)
       case AddEntity(_, typeof, entity)     => handler.addEntity(typeof, entity)
@@ -38,27 +38,3 @@ final class Dispatcher(emailer: Emailer,
         }.recover { case NonFatal(error) => Fault(s"Authorization failed: $error") }
          .get
       case Register(_) | Login(_, _) => Authorized(true)
-
-  private def send(email: String,
-                   message: String): Unit =
-    val recipients = List(email)
-    emailer.send(recipients, message)
-
-  private def register(email: String): Event =
-    Try {
-      val account = Account(email = email)
-      val message = s"<p><b>Account Registration:</b> Your new pin is: <b>${account.pin}</b> Welcome aboard!</p>"
-      send(account.email, message)
-      val id = store.register(account)
-      Registered( account.copy(id = id) )
-    }.recover { case NonFatal(error) => Fault(s"Registration failed for: $email, because: ${error.getMessage}") }
-     .get
-
-  private def login(email: String,
-                    pin: String): Event =
-    Try { store.login(email, pin) }.fold(
-      error => Fault(s"Login failed: ${error.getMessage()}"),
-      optionalAccount =>
-        if optionalAccount.isDefined then LoggedIn(optionalAccount.get)
-        else Fault(s"Login failed for email address: $email and pin: $pin")
-    )
--- src/main/scala/objektwerks/Handler.scala
+++ src/main/scala/objektwerks/Handler.scala
@@ -1,6 +1,35 @@
 package objektwerks
 
+import scala.util.Try
+import scala.util.control.NonFatal
+
+import Validator.*
+
 final class Handler(store: Store):
+  def send(email: String,
+                   message: String): Unit =
+    val recipients = List(email)
+    emailer.send(recipients, message)
+
+  def register(email: String): Event =
+    Try {
+      val account = Account(email = email)
+      val message = s"<p><b>Account Registration:</b> Your new pin is: <b>${account.pin}</b> Welcome aboard!</p>"
+      send(account.email, message)
+      val id = store.register(account)
+      Registered( account.copy(id = id) )
+    }.recover { case NonFatal(error) => Fault(s"Registration failed for: $email, because: ${error.getMessage}") }
+     .get
+
+  def login(email: String,
+                    pin: String): Event =
+    Try { store.login(email, pin) }.fold(
+      error => Fault(s"Login failed: ${error.getMessage()}"),
+      optionalAccount =>
+        if optionalAccount.isDefined then LoggedIn(optionalAccount.get)
+        else Fault(s"Login failed for email address: $email and pin: $pin")
+    )
+
   def listEntities(typeof: EntityType, houseId: Long): Event = ???
 
   def addEntity(typeof: EntityType, entity: Entity): Event = ???

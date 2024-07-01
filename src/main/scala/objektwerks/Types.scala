0363703f57890f668668ef03df5b4f3b3ca1b844
objektwerks <objektwerks@runbox.com>
2024-07-01 18:12:46 -0400

    edited types

--- src/main/scala/objektwerks/Types.scala
+++ src/main/scala/objektwerks/Types.scala
@@ -1,8 +1,8 @@
 package objektwerks
 
 enum EntityType:
-  case House, Foundation, Frame, Attic, Insulation, Ductwork, Ventilation, Roof, Chimney, Balcony, Drywall, Room, Driveway, Garage,
-  Siding, Gutter, Soffit, Window, Door, Plumbing, Electrical, Fusebox, Alarm, Heater, AirConditioner, Floor, Lighting,
+  case House, Foundation, Frame, Attic, Insulation, Ductwork, Ventilation, Roof, Chimney, Balcony, Drywall, Room, Driveway,
+  Garage, Siding, Gutter, Soffit, Window, Door, Plumbing, Electrical, Fusebox, Alarm, Heater, AirConditioner, Floor, Lighting,
   Sewage, Well, Water, WaterHeater, Lawn, Garden, Sprinkler, Shed, SolarPanel, Porch, Patio, Pool, Dock, Gazebo, Mailbox
 
 enum HouseType:

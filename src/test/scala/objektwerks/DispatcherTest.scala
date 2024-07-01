760e32ebd884d900a5ac01d5c523ebada1bf3cb5
objektwerks <objektwerks@runbox.com>
2024-07-01 15:52:26 -0400

    enhanced dispatcher test

--- src/test/scala/objektwerks/DispatcherTest.scala
+++ src/test/scala/objektwerks/DispatcherTest.scala
@@ -7,6 +7,8 @@ import org.scalatest.matchers.should.Matchers
 
 import scala.sys.process.Process
 
+import Validator.*
+
 final class DispatcherTest extends AnyFunSuite with Matchers:
   val exitCode = Process("psql -d house -f ddl.sql").run().exitValue()
   exitCode shouldBe 0
@@ -15,9 +17,20 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
   val cache = Store.cache(config)
   val datasource = Store.datasource(config)
   val store = Store(cache, datasource)
+  val emailer = Emailer(config)
+  val handler = Handler(store, emailer)
+  val dispatcher = Dispatcher(handler)
 
   var testAccount = Account(email = "your@email.com")
   var testHouse = House(accountId = 0, location = "100 Rocky Way")
 
   test("store"):
-    true
+    register
+
+  def register: Unit =
+    val register = Register(config.getString("email.sender"))
+    dispatcher.dispatch(register) match
+      case Registered(account) =>
+        assert( account.isValid )
+        testAccount = account
+      case fault => fail(s"Invalid registered event: $fault")

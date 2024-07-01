2bdcc1e89c02ad52c596b83beaf013e29bc047b4
objektwerks <objektwerks@runbox.com>
2024-07-01 11:28:41 -0400

    enhanced list function map

--- src/main/scala/objektwerks/Handler.scala
+++ src/main/scala/objektwerks/Handler.scala
@@ -9,7 +9,8 @@ final class Handler(store: Store,
                     emailer: Emailer):
   val list = Map(
     Foundation -> listFoundations, Frame -> listFrames, Attic -> listAttics, Insulation -> listInsulations,
-    Ductwork -> listDuctworks, Ventilation -> listVentilations, Roof -> listRoofs, Chimney -> listChimneys
+    Ductwork -> listDuctworks, Ventilation -> listVentilations, Roof -> listRoofs, Chimney -> listChimneys,
+    Balcony -> listBalconys
   )
   val add = Map(
     House -> addHouse, Foundation -> addFoundation, Frame -> addFrame, Attic -> addAttic, Insulation -> addInsulation,

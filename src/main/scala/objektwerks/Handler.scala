614609474b5e57d07761c59e2382a1d87620083f
objektwerks <objektwerks@runbox.com>
2024-07-03 08:24:53 -0400

    enhanced list function map

--- src/main/scala/objektwerks/Handler.scala
+++ src/main/scala/objektwerks/Handler.scala
@@ -12,7 +12,8 @@ final class Handler(store: Store,
     Ductwork -> listDuctworks, Ventilation -> listVentilations, Roof -> listRoofs, Chimney -> listChimneys, Balcony -> listBalconys,
     Drywall -> listDrywalls, Room -> listRooms, Driveway -> listDriveways, Garage -> listGarages, Siding -> listSidings,
     Gutter -> listGutters, Soffit -> listSoffits, Window -> listWindows, Door -> listDoors,     Plumbing -> listPlumbings,
-    Electrical -> listElectricals, Fusebox -> listFuseboxes, Alarm -> listAlarms, Heater -> listHeaters
+    Electrical -> listElectricals, Fusebox -> listFuseboxes, Alarm -> listAlarms, Heater -> listHeaters,
+    AirConditioner -> listAirConditioners
   )
   val add = Map(
     House -> addHouse, Foundation -> addFoundation, Frame -> addFrame, Attic -> addAttic, Insulation -> addInsulation,

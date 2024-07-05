173be586b555298dd070567f991db3ae5c946750
objektwerks <objektwerks@runbox.com>
2024-07-05 07:32:46 -0400

    enhanced update function map

--- src/main/scala/objektwerks/Handler.scala
+++ src/main/scala/objektwerks/Handler.scala
@@ -37,7 +37,7 @@ final class Handler(store: Store,
     Heater -> updateHeater, AirConditioner -> updateAirConditioner, Floor -> updateFloor, Lighting -> updateLighting,
     Sewage -> updateSewage, Well -> updateWell, Water -> updateWater, WaterHeater -> updateWaterHeater, Lawn -> updateLawn,
     Garden -> updateGarden, Sprinkler -> updateSprinkler, Shed -> updateShed, SolarPanel -> updateSolarPanel, Porch -> updatePorch,
-    Patio -> updatePatio
+    Patio -> updatePatio, Pool -> updatePool
   )
   def isAuthorized(command: Command): Event =
     command match

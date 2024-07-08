383e65d37df3c2d396e850d8947c94e5b65867e8
objektwerks <objektwerks@runbox.com>
2024-07-08 08:08:59 -0400

    added list ventilations

--- src/test/scala/objektwerks/DispatcherTest.scala
+++ src/test/scala/objektwerks/DispatcherTest.scala
@@ -205,3 +205,11 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
     dispatcher.dispatch(updateEntity) match
       case EntityUpdated(count) => count shouldBe 1
       case fault => fail(s"Invalid ventialtion updated event: $fault")
+
+  def listVentilations: Unit =
+    val list = ListEntities(testAccount.license, EntityType.Ventilation, testVentilation.id)
+    dispatcher.dispatch(list) match
+      case EntitiesListed(list) =>
+        list.length shouldBe 1
+        list.head shouldBe testVentilation
+      case fault => fail(s"Invalid ventilation listed event: $fault")

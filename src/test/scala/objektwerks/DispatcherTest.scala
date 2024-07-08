7f36962c616328b62fc36ff751d97f3a3b4f02e1
objektwerks <objektwerks@runbox.com>
2024-07-08 08:25:51 -0400

    added list chimneys

--- src/test/scala/objektwerks/DispatcherTest.scala
+++ src/test/scala/objektwerks/DispatcherTest.scala
@@ -253,3 +253,11 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
     dispatcher.dispatch(updateEntity) match
       case EntityUpdated(count) => count shouldBe 1
       case fault => fail(s"Invalid chimney updated event: $fault")
+
+  def listChimneys: Unit =
+    val list = ListEntities(testAccount.license, EntityType.Chimney, testChimney.id)
+    dispatcher.dispatch(list) match
+      case EntitiesListed(list) =>
+        list.length shouldBe 1
+        list.head shouldBe testChimney
+      case fault => fail(s"Invalid chimney listed event: $fault")

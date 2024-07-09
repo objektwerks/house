1b9c2f068b494f662590413ad57cf2318fff1b97
objektwerks <objektwerks@runbox.com>
2024-07-09 07:30:06 -0400

    added list drywalls

--- src/test/scala/objektwerks/DispatcherTest.scala
+++ src/test/scala/objektwerks/DispatcherTest.scala
@@ -368,3 +368,11 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
     dispatcher.dispatch(updateEntity) match
       case EntityUpdated(count) => count shouldBe 1
       case fault => fail(s"Invalid drywall updated event: $fault")
+
+  def listDrywalls: Unit =
+    val list = ListEntities(testAccount.license, EntityType.Drywall, testDrywall.id)
+    dispatcher.dispatch(list) match
+      case EntitiesListed(list) =>
+        list.length shouldBe 1
+        list.head shouldBe testDrywall
+      case fault => fail(s"Invalid drywall listed event: $fault")

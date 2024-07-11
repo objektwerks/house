8620c1031b3e3a1ac569b37b8ce583bdf192be19
objektwerks <objektwerks@runbox.com>
2024-07-11 08:27:29 -0400

    added list sidings

--- src/test/scala/objektwerks/DispatcherTest.scala
+++ src/test/scala/objektwerks/DispatcherTest.scala
@@ -476,3 +476,11 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
     dispatcher.dispatch(updateEntity) match
       case EntityUpdated(count) => count shouldBe 1
       case fault => fail(s"Invalid siding updated event: $fault")
+
+  def listSidings: Unit =
+    val list = ListEntities(testAccount.license, EntityType.Siding, testSiding.id)
+    dispatcher.dispatch(list) match
+      case EntitiesListed(list) =>
+        list.length shouldBe 1
+        list.head shouldBe testSiding
+      case fault => fail(s"Invalid siding listed event: $fault")

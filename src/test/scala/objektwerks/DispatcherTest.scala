9a433af91471ecb8bb57b8639ff688b0532ef80f
objektwerks <objektwerks@runbox.com>
2024-07-11 09:09:40 -0400

    added list electricals

--- src/test/scala/objektwerks/DispatcherTest.scala
+++ src/test/scala/objektwerks/DispatcherTest.scala
@@ -638,3 +638,11 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
     dispatcher.dispatch(updateEntity) match
       case EntityUpdated(count) => count shouldBe 1
       case fault => fail(s"Invalid electrical updated event: $fault")
+
+  def listElectricals: Unit =
+    val list = ListEntities(testAccount.license, EntityType.Electrical, testElectrical.id)
+    dispatcher.dispatch(list) match
+      case EntitiesListed(list) =>
+        list.length shouldBe 1
+        list.head shouldBe testElectrical
+      case fault => fail(s"Invalid electrical listed event: $fault")

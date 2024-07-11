db08118b7d9e5e77b5926e37b5d91774d890294f
objektwerks <objektwerks@runbox.com>
2024-07-11 09:16:00 -0400

    added list fuseboxes

--- src/test/scala/objektwerks/DispatcherTest.scala
+++ src/test/scala/objektwerks/DispatcherTest.scala
@@ -661,3 +661,11 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
     dispatcher.dispatch(updateEntity) match
       case EntityUpdated(count) => count shouldBe 1
       case fault => fail(s"Invalid fusebox updated event: $fault")
+
+  def listFuseboxes: Unit =
+    val list = ListEntities(testAccount.license, EntityType.Fusebox, testFusebox.id)
+    dispatcher.dispatch(list) match
+      case EntitiesListed(list) =>
+        list.length shouldBe 1
+        list.head shouldBe testFusebox
+      case fault => fail(s"Invalid fusebox listed event: $fault")

24af32ee3f779593c0e75f7854e06583da842d42
objektwerks <objektwerks@runbox.com>
2024-07-11 09:24:47 -0400

    added list heaters

--- src/test/scala/objektwerks/DispatcherTest.scala
+++ src/test/scala/objektwerks/DispatcherTest.scala
@@ -715,3 +715,11 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
     dispatcher.dispatch(updateEntity) match
       case EntityUpdated(count) => count shouldBe 1
       case fault => fail(s"Invalid heater updated event: $fault")
+
+  def listHeater: Unit =
+    val list = ListEntities(testAccount.license, EntityType.Heater, testHeater.id)
+    dispatcher.dispatch(list) match
+      case EntitiesListed(list) =>
+        list.length shouldBe 1
+        list.head shouldBe testHeater
+      case fault => fail(s"Invalid heater listed event: $fault")

30c6fa1a26601fad4562a8916a1a624c4f24511f
objektwerks <objektwerks@runbox.com>
2024-07-12 06:49:01 -0400

    added list gardens

--- src/test/scala/objektwerks/DispatcherTest.scala
+++ src/test/scala/objektwerks/DispatcherTest.scala
@@ -954,3 +954,11 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
     dispatcher.dispatch(updateEntity) match
       case EntityUpdated(count) => count shouldBe 1
       case fault => fail(s"Invalid garden updated event: $fault")
+
+  def listGardens: Unit =
+    val list = ListEntities(testAccount.license, EntityType.Garden, testGarden.id)
+    dispatcher.dispatch(list) match
+      case EntitiesListed(list) =>
+        list.length shouldBe 1
+        list.head shouldBe testGarden
+      case fault => fail(s"Invalid garden listed event: $fault")

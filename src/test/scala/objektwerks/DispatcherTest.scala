063784d400b7c6d58d28732d6eb580a0fe3ec99c
objektwerks <objektwerks@runbox.com>
2024-07-12 07:25:54 -0400

    enhanced dispatcher test

--- src/test/scala/objektwerks/DispatcherTest.scala
+++ src/test/scala/objektwerks/DispatcherTest.scala
@@ -221,6 +221,10 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
     updatePool
     listPools
 
+    addDock
+    updateDock
+    listDocks
+
   def register: Unit =
     val register = Register(config.getString("email.sender"))
     dispatcher.dispatch(register) match

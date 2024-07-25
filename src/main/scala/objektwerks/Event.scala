5894c8bc940c18219688a83c26259f695bafd2eb
objektwerks <objektwerks@runbox.com>
2024-07-25 06:41:52 -0400

    remvoed unused event

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -6,8 +6,6 @@ final case class Registered(account: Account) extends Event
 
 final case class LoggedIn(account: Account) extends Event
 
-final case class HousesListed(houses: List[House]) extends Event
-
 final case class EntitiesListed(entities: List[Entity]) extends Event
 
 final case class EntityAdded(id: Long) extends Event

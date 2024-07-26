71014d12d6a0c885b4f7cf77a0d965a6d6c40a27
objektwerks <objektwerks@runbox.com>
2024-07-26 15:55:58 -0400

    refactored pin

--- src/main/scala/objektwerks/Pin.scala
+++ src/main/scala/objektwerks/Pin.scala
@@ -1,7 +1,8 @@
 package objektwerks
 
+import scala.annotation.tailrec
 import scala.collection.mutable
-import scala.util.Random
+import scala.util.{Failure, Random, Success, Try}
 
 object Pin:
   private val chars = "abcdefghijklmnopqrstuvwxyz"
@@ -11,7 +12,13 @@ object Pin:
   private def newChar: Char = chars( random.nextInt(chars.length) )
   private def newSpecialChar: Char = specialChars( random.nextInt(specialChars.length) )
 
-  def newInstance: String =
+  @tailrec
+  private def retry[T](attempts: Int)(fn: => T): T =
+    Try( fn ) match
+      case Success(result) => result
+      case Failure(error) => if attempts >= 1 then retry(attempts - 1)(fn) else throw error
+
+  def build: String =
     val buffer = mutable.ArrayBuffer.empty[Char]
 
     buffer += newSpecialChar
@@ -24,3 +31,5 @@ object Pin:
 
     val pin = buffer.mkString
     if pin.length < 7 then throw Exception(pin) else pin
+
+  def newInstance: String = retry(3)(build)

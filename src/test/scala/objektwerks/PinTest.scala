e6af0743ea9c74724100f10ef3ba91201ba6faeb
objektwerks <objektwerks@runbox.com>
2024-07-26 15:30:56 -0400

    enhanced pin test

--- src/test/scala/objektwerks/PinTest.scala
+++ src/test/scala/objektwerks/PinTest.scala
@@ -8,9 +8,9 @@ import scala.collection.mutable
 final class PinTest extends AnyFunSuite with Matchers:
   test("pin"):
     val pins = mutable.Set.empty[String]
-    for i <- 1 to 10_000
+    for i <- 1 to 10_000_000
     do
       val pin = Pin.newInstance
       pin.length shouldBe 7
       pins += pin
-    pins.size shouldBe 10_000
+    pins.size shouldBe 10_000_000

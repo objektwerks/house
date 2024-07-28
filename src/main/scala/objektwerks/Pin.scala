109f47f4fc8f47cda7df6e44f31dba3a981b3bf4
objektwerks <objektwerks@runbox.com>
2024-07-28 14:15:17 -0400

    refactored pin

--- src/main/scala/objektwerks/Pin.scala
+++ src/main/scala/objektwerks/Pin.scala
@@ -4,17 +4,19 @@ import scala.collection.mutable
 import scala.util.Random
 
 object Pin:
+  private val numbers = "0123456789"
   private val chars = "abcdefghijklmnopqrstuvwxyz"
   private val specialChars = "~!@#$%^&*-+=<>?/:;"
   private val random = Random
 
+  private def newNumber: Char = numbers( random.nextInt(numbers.length).toChar )
   private def newChar: Char = chars( random.nextInt(chars.length) )
   private def newSpecialChar: Char = specialChars( random.nextInt(specialChars.length) )
 
   def newInstance: String =
     val pin = mutable.ArrayBuffer[Char]()
 
-    pin += newSpecialChar
+    pin += newNumber
     pin += newChar.toUpper
     pin += newChar.toLower
     pin += newChar.toUpper

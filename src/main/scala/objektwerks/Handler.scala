c17575fe3f4eaea98d2e5ec0872e3394763b2202
objektwerks <objektwerks@runbox.com>
2024-08-14 14:18:27 -0400

    added list issues to list function map

--- src/main/scala/objektwerks/Handler.scala
+++ src/main/scala/objektwerks/Handler.scala
@@ -8,7 +8,7 @@ import EntityType.*
 final class Handler(store: Store,
                     emailer: Emailer):
   val list = Map(
-    House -> listHouses, Drawing -> listDrawings, Foundation -> listFoundations, Frame -> listFrames, Attic -> listAttics,
+    House -> listHouses, Issue -> listIssues, Drawing -> listDrawings, Foundation -> listFoundations, Frame -> listFrames, Attic -> listAttics,
     Insulation -> listInsulations, Ductwork -> listDuctworks, Ventilation -> listVentilations, Roof -> listRoofs,
     Chimney -> listChimneys, Balcony -> listBalconys, Drywall -> listDrywalls, Room -> listRooms, Driveway -> listDriveways,
     Garage -> listGarages, Siding -> listSidings, Gutter -> listGutters, Soffit -> listSoffits, Window -> listWindows,

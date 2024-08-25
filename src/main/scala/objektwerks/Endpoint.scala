b0f58268fe93a9544bb42c1f51727a0d8f42d075
objektwerks <objektwerks@runbox.com>
2024-08-25 10:35:45 -0400

    enhanced endpoint; edited build.sbt

--- src/main/scala/objektwerks/Endpoint.scala
+++ src/main/scala/objektwerks/Endpoint.scala
@@ -1,12 +1,12 @@
 package objektwerks
 
+import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
 import ox.{ExitCode, IO, Ox, OxApp}
 
 import sttp.tapir.*
-import sttp.tapir.json.jsoniter.*
 
 import Serializer.given
 
@@ -17,18 +17,31 @@ object Endpoint extends OxApp with LazyLogging:
     val port = config.getInt("server.port")
     val path = config.getString("server.endpoint")
 
+    val store = Store(config)
+    val emailer = Emailer(config)
+    val handler = Handler(store, emailer)
+    val dispatcher = Dispatcher(handler)
+
     println(s"*** Press Control-C to shutdown House Http Server at: $host:$port$path")
     logger.info(s"*** House Http Server started at: $host:$port$path")
 
     endpoint
       .post
-      .in(jsonBody[Command])
-      .out(jsonBody[Event])
-      .handle { command => 
-        logger.info(s"*** Endpoint command: $command")
+      .in(path)
+      .in(stringBody)
+      .out(stringBody)
+      .handleSuccess { commandJson => 
+        logger.info(s"*** command json: $commandJson")
+
+        val command = readFromString[Command](commandJson)
+        logger.info(s"*** command: $command")
+
         val event = dispatcher.dispatch(command)
-        logger.info(s"*** Dispatcher event: $event")
-        event
+        logger.info(s"*** event: $event")
+
+        val eventJson = writeToString[Event](event)
+        logger.info(s"*** event json: $eventJson")
+        eventJson
       }
 
     ExitCode.Success

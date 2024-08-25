9272836193e478582f2af239fb2259ab9be5bcf5
objektwerks <objektwerks@runbox.com>
2024-08-25 10:40:29 -0400

    enhanced endpoint

--- src/main/scala/objektwerks/Endpoint.scala
+++ src/main/scala/objektwerks/Endpoint.scala
@@ -4,9 +4,12 @@ import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
+import java.util.concurrent.Executors
+
 import ox.{ExitCode, IO, Ox, OxApp}
 
 import sttp.tapir.*
+import sttp.tapir.server.jdkhttp.JdkHttpServer
 
 import Serializer.given
 
@@ -22,26 +25,36 @@ object Endpoint extends OxApp with LazyLogging:
     val handler = Handler(store, emailer)
     val dispatcher = Dispatcher(handler)
 
+    val commandEndpoint =
+      endpoint
+        .post
+        .in(path)
+        .in(stringBody)
+        .out(stringBody)
+        .handleSuccess { commandJson => 
+          logger.info(s"*** command json: $commandJson")
+          val command = readFromString[Command](commandJson)
+          logger.info(s"*** command: $command")
+
+          val event = dispatcher.dispatch(command)
+          logger.info(s"*** event: $event")
+
+          val eventJson = writeToString[Event](event)
+          logger.info(s"*** event json: $eventJson")
+          eventJson
+        }
+
+    val jdkHttpServer = JdkHttpServer()
+      .executor( Executors.newVirtualThreadPerTaskExecutor() )
+      .host("localhost")
+      .port(7777)
+      .addEndpoint(commandEndpoint)
+      .start()
+
     println(s"*** Press Control-C to shutdown House Http Server at: $host:$port$path")
     logger.info(s"*** House Http Server started at: $host:$port$path")
 
-    endpoint
-      .post
-      .in(path)
-      .in(stringBody)
-      .out(stringBody)
-      .handleSuccess { commandJson => 
-        logger.info(s"*** command json: $commandJson")
+    Thread.currentThread().join()
 
-        val command = readFromString[Command](commandJson)
-        logger.info(s"*** command: $command")
-
-        val event = dispatcher.dispatch(command)
-        logger.info(s"*** event: $event")
-
-        val eventJson = writeToString[Event](event)
-        logger.info(s"*** event json: $eventJson")
-        eventJson
-      }
-
+    jdkHttpServer.stop(0)
     ExitCode.Success

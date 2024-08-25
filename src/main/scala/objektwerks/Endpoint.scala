3148d0d5f8f13fd589d8d86695f7d71f682a9522
objektwerks <objektwerks@runbox.com>
2024-08-25 09:55:47 -0400

    enhanced endpoint

--- src/main/scala/objektwerks/Endpoint.scala
+++ src/main/scala/objektwerks/Endpoint.scala
@@ -5,7 +5,10 @@ import com.typesafe.scalalogging.LazyLogging
 
 import ox.{ExitCode, IO, Ox, OxApp}
 
-//import Serializer.given
+import sttp.tapir.*
+import sttp.tapir.json.jsoniter.*
+
+import Serializer.given
 
 object Endpoint extends OxApp with LazyLogging:
   override def run(args: Vector[String])(using Ox, IO): ExitCode =
@@ -16,5 +19,16 @@ object Endpoint extends OxApp with LazyLogging:
 
     println(s"*** Press Control-C to shutdown House Http Server at: $host:$port$path")
     logger.info(s"*** House Http Server started at: $host:$port$path")
+
+    endpoint
+      .post
+      .in(jsonBody[Command])
+      .out(jsonBody[Event])
+      .handle { command => 
+        logger.info(s"*** Endpoint command: $command")
+        val event = dispatcher.dispatch(command)
+        logger.info(s"*** Dispatcher event: $event")
+        event
+      }
 
     ExitCode.Success

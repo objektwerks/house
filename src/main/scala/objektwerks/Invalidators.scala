15233be5e6eaefd8b55039b1a73b97de4a99fc15
objektwerks <objektwerks@runbox.com>
2024-09-09 14:52:37 -0400

    enhanced invalidators

--- src/main/scala/objektwerks/Invalidators.scala
+++ src/main/scala/objektwerks/Invalidators.scala
@@ -18,6 +18,17 @@ object Invalidators:
         .invalidate(account.pin.isPin)(Field("pin"), Message("Must be 7 characters in length."))
         .invalidate(account.activated.nonEmpty)(Field("activated"), Message("Must be non empty."))
 
+  extension (house: House)
+    def invalidate: Invalidator =
+      Invalidator()
+        .invalidate(house.id >= 0)(Field("id"), Message("Must be greater than or equal to 0."))
+        .invalidate(house.accountId > 0)(Field("accountId"), Message("Must be greater than 0."))
+        .invalidate(house.label.isEmptyOrNonEmpty)(Field("label"), Message("Must be empty or non empty."))
+        .invalidate(house.note.isEmptyOrNonEmpty)(Field("note"), Message("Must be empty or non empty."))
+        .invalidate(house.architect.isEmptyOrNonEmpty)(Field("architect"), Message("Must be empty or non empty."))
+        .invalidate(house.builder.isEmptyOrNonEmpty)(Field("builder"), Message("Must be empty or non empty."))
+        .invalidate(house.built.nonEmpty)(Field("built"), Message("Must be non empty."))
+
   extension (common: Common)
     def invalidate: Invalidator =
       Invalidator()

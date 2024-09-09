0e2062b5c41b83f24c2678a45bba7e470fabe7d7
objektwerks <objektwerks@runbox.com>
2024-09-09 13:24:08 -0400

    added invalidator

---
+++ src/main/scala/objektwerks/Invalidator.scala
@@ -0,0 +1,30 @@
+package objektwerks
+
+import scala.collection.mutable
+
+final class Invalidator:
+  private val invalidations = mutable.Map[Field, Message]()
+
+  private def add(field: Field, message: Message): Invalidator =
+    invalidations += field -> message
+    this
+
+  def isValid: Boolean = invalidations.isEmpty
+
+  def isInvalid: Boolean = !invalidations.isEmpty
+
+  def count: Int = invalidations.size
+
+  def asList: List[String] = invalidations.map { (_, message) => s"$message" }.toList
+
+  def asMap: Map[Field, Message] = invalidations.toMap
+
+  def asString(separator: String = ","): String = asList.mkString(separator)
+
+  def invalidate(isInvalidExpr: Boolean)(field: Field, message: Message): Invalidator =
+    if isInvalidExpr then add(field, message)
+    else this
+
+  def invalidate(isInvalidFn: () => Boolean)(field: Field, message: Message): Invalidator =
+    if isInvalidFn() then add(field, message)
+    else this

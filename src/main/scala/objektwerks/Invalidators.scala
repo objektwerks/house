5ee7dba07b36c1cdfd4f4dd1516005d6db483008
objektwerks <objektwerks@runbox.com>
2024-09-09 14:11:27 -0400

    enhanced invalidators

--- src/main/scala/objektwerks/Invalidators.scala
+++ src/main/scala/objektwerks/Invalidators.scala
@@ -1,4 +1,13 @@
 package objektwerks
 
+import Invalidator.*
+import Validators.* 
+
 object Invalidators:
-  
+  extension (common: Common)
+    def invalidate: Invalidator =
+      Invalidator()
+        .invalidate(common.id >= 0)(Field("id"), Message("Must be greater than or equal to 0."))
+        .invalidate(common.houseId > 0)(Field("houseId"), Message("Must be great than 0."))
+        .invalidate(common.label.isEmptyOrNonEmpty)(Field("label"), Message("Must be empty or non empty."))
+        .invalidate(common.note.isEmptyOrNonEmpty)(Field("note"), Message("Must be empty or non empty."))

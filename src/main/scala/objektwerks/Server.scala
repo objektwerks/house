c335488239e0a609757abc610cc0a582c5c07e32
objektwerks <objektwerks@runbox.com>
2024-09-09 09:30:47 -0400

    refactored server

--- src/main/scala/objektwerks/Server.scala
+++ src/main/scala/objektwerks/Server.scala
@@ -7,14 +7,8 @@ import java.util.concurrent.Executors
 
 import ox.{ExitCode, IO, never, Ox, OxApp, releaseAfterScope}
 
-import sttp.shared.Identity
 import sttp.tapir.*
-import sttp.tapir.json.jsoniter.*
 import sttp.tapir.server.jdkhttp.JdkHttpServer
-import sttp.tapir.swagger.bundle.SwaggerInterpreter
-
-import Serializer.given
-import Schemas.given
 
 object Server extends OxApp with LazyLogging:
   override def run(args: Vector[String])(using Ox, IO): ExitCode =
@@ -27,18 +21,19 @@ object Server extends OxApp with LazyLogging:
     val emailer = Emailer(config)
     val handler = Handler(store, emailer)
     val dispatcher = Dispatcher(handler)
+    val endpoints = Endpoints(path, dispatcher)
 
     val jdkHttpServer = JdkHttpServer()
       .executor( Executors.newVirtualThreadPerTaskExecutor() )
       .host(host)
       .port(port)
-      .addEndpoint(commandEndpoint)
-      .addEndpoints(swaggerEndpoints)
+      .addEndpoint(endpoints.commandEndpoint)
+      .addEndpoints(endpoints.swaggerEndpoints)
       .start()
 
-    println(s"*** House Endpoint: ${commandEndpoint.show}")
+    println(s"*** House Endpoint: ${endpoints.commandEndpoint.show}")
     println(s"*** Press Control-C to shutdown House Http Server at: $host:$port/$path")
-    logger.info(s"*** House Endpoint: ${commandEndpoint.show}")
+    logger.info(s"*** House Endpoint: ${endpoints.commandEndpoint.show}")
     logger.info(s"*** House Http Server started at: $host:$port/$path")
 
     releaseAfterScope:
